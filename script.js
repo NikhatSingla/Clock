8780e3ff3f408d925ffac98cebbcdf71c0938144
Nikhat Singla <singlanikhat@gmail.com>
2024-06-07 16:08:32 +0530

    Updated to fetch time every second.

--- script.js
+++ script.js
@@ -2,34 +2,16 @@ const hourHandRotate = document.getElementsByClassName("hour")[0].style;
 const minuteHandRotate = document.getElementsByClassName("minute")[0].style;
 const secondHandRotate = document.getElementsByClassName("second")[0].style;
 
-let time = new Date();
-let hour = time.getHours();
-let minute = time.getMinutes();
-let second = time.getSeconds();
-
 function changeTime() {
-    if (second === 60) {
-        second = 0;
-        minute += 1;
-    }
-
-    if (minute === 60) {
-        minute = 0;
-        hour += 1;
-    }
-
-    if (hour >= 12) {
-        hour = hour - 12;
-    }
+    
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
 
     hourHandRotate.transform = "rotateZ(" + (30 * hour + 0.5 * minute) + "deg)";
     minuteHandRotate.transform = "rotateZ(" + (6 * minute + 0.1 * second) + "deg)";
     secondHandRotate.transform = "rotateZ(" + (6 * second) + "deg)";
-    
-    console.log("rotateZ(" + 30 * hour + 0.5 * minute + "deg)");
-    
-    ++second;
-    setTimeout(changeTime, 1000);
 }
 
-changeTime();
+setInterval(changeTime, 1000);
