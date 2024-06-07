4e14f5e1b01c79bee4d2c75cb3df9d3d8c61427b
Nikhat Singla <singlanikhat@gmail.com>
2024-06-07 16:14:37 +0530

    Updated look and added display time.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const hourHandRotate = document.getElementsByClassName("hour")[0].style;
 const minuteHandRotate = document.getElementsByClassName("minute")[0].style;
 const secondHandRotate = document.getElementsByClassName("second")[0].style;
 
+const footer = document.getElementsByClassName("footer")[0];
+
 function changeTime() {
     
     let time = new Date();
@@ -12,6 +14,8 @@ function changeTime() {
     hourHandRotate.transform = "rotateZ(" + (30 * hour + 0.5 * minute) + "deg)";
     minuteHandRotate.transform = "rotateZ(" + (6 * minute + 0.1 * second) + "deg)";
     secondHandRotate.transform = "rotateZ(" + (6 * second) + "deg)";
+
+    footer.innerText = hour + ":" + minute + ":" + second;
 }
 
 setInterval(changeTime, 1000);
